5382c77a67b245954456923933ef1bdfcca259e0
Nico Vermeir <nico_vermeir@hotmail.com>
2016-12-14 07:43:37 +0100

    Added styled Switch widget

--- OpenHAB.Windows/Controls/SwitchWidget.xaml.cs
+++ OpenHAB.Windows/Controls/SwitchWidget.xaml.cs
@@ -2,7 +2,7 @@
 using OpenHAB.Core.Messages;
 using OpenHAB.Core.Model;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 namespace OpenHAB.Windows.Controls
 {
@@ -12,6 +12,26 @@ namespace OpenHAB.Windows.Controls
     public sealed partial class SwitchWidget : WidgetBase
     {
         /// <summary>
+        /// Property to bind the toggle state to
+        /// </summary>
+        public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register(
+            "IsOn", typeof(bool), typeof(SwitchWidget), new PropertyMetadata(default(bool), PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            ((SwitchWidget)dependencyObject).OnToggle();
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the toggle is on or off
+        /// </summary>
+        public bool IsOn
+        {
+            get { return (bool)GetValue(IsOnProperty); }
+            set { SetValue(IsOnProperty, value); }
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="SwitchWidget"/> class.
         /// </summary>
         public SwitchWidget()
@@ -19,9 +39,14 @@ namespace OpenHAB.Windows.Controls
             InitializeComponent();
         }
 
-        private void ToggleSwitch_OnToggled(object sender, RoutedEventArgs e)
+        private void OnToggle()
         {
-            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, ((ToggleSwitch)sender).IsOn ? OpenHABCommands.OnCommand : OpenHABCommands.OffCommand));
+            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, IsOn ? OpenHABCommands.OnCommand : OpenHABCommands.OffCommand));
+        }
+
+        private void OnToggle(object sender, TappedRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, ToggleStates.CurrentState == OnState ? "OffState" : "OnState", true);
         }
     }
 }
