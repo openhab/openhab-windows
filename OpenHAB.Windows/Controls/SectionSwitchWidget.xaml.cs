f9d11c6cc9762182ecb44c63393d8eb8fd658cb9
NicoVermeir <nico_vermeir@hotmail.com>
2016-12-20 20:42:43 +0100

    Added section switch widget (#14)

---
+++ OpenHAB.Windows/Controls/SectionSwitchWidget.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace OpenHAB.Windows.Controls
+{
+    /// <summary>
+    /// Widget control that represents an OpenHAB switch
+    /// </summary>
+    public sealed partial class SectionSwitchWidget : WidgetBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SectionSwitchWidget"/> class.
+        /// </summary>
+        public SectionSwitchWidget()
+        {
+            InitializeComponent();
+        }
+    }
+}
