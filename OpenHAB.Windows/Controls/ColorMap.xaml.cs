7e0c95192a2194a3609df2a0466ddda0383f8960
Nico Vermeir <nico_vermeir@hotmail.com>
2016-12-27 20:02:47 +0100

    added colorchanged handling and default value

--- OpenHAB.Windows/Controls/ColorMap.xaml.cs
+++ OpenHAB.Windows/Controls/ColorMap.xaml.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using OpenHAB.Core.Common;
 using Windows.Foundation;
 using Windows.UI;
 using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
@@ -17,20 +17,30 @@ using ColorHelper = OpenHAB.Core.Common.ColorHelper;
 
 namespace OpenHAB.Windows.Controls
 {
+    /// <summary>
+    /// Colorpicker control
+    /// </summary>
     public sealed partial class ColorMap : UserControl
     {
-        private PointerPoint lastPoint;
-        private double colorX, colorY;
-        private WriteableBitmap bmp3;
-
-        private readonly LinearGradientBrush _lightnessGradient;
         private readonly GradientStop _lightnessStart;
         private readonly GradientStop _lightnessMid;
         private readonly GradientStop _lightnessEnd;
 
+        private PointerPoint _lastPoint;
+        private WriteableBitmap _bitmap;
+        private double _colorX;
+        private double _colorY;
         private bool _settingColor;
-        private bool _settingLightness;
+        private bool _isloaded;
 
+        /// <summary>
+        /// Event that fires whenever a new color is selected
+        /// </summary>
+        public event EventHandler<ColorChangedEventArgs> ColorChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorMap"/> class.
+        /// </summary>
         public ColorMap()
         {
             InitializeComponent();
@@ -41,149 +51,162 @@ namespace OpenHAB.Windows.Controls
             ellipse.PointerPressed += Image3_PointerPressed;
             ellipse.PointerReleased += Image3_PointerReleased;
 
-            _lightnessGradient = new LinearGradientBrush
+            var lightnessGradient = new LinearGradientBrush
             {
                 StartPoint = new Point(0, 0),
                 EndPoint = new Point(0, 1)
             };
+
             _lightnessStart = new GradientStop();
             _lightnessMid = new GradientStop { Offset = 0.5 };
             _lightnessEnd = new GradientStop { Offset = 1 };
-            _lightnessGradient.GradientStops = new GradientStopCollection()
+            lightnessGradient.GradientStops = new GradientStopCollection()
             {
                 _lightnessStart, _lightnessMid, _lightnessEnd,
             };
-            //this.LightnessBackground.Fill = this._lightnessGradient;
         }
 
+        /// <summary>
+        /// Gets or sets the Color property
+        /// </summary>
         public Color Color
         {
             get { return (Color)GetValue(ColorProperty); }
             set { SetValue(ColorProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Color.  This enables animation, styling, binding, etc...
+        /// <summary>
+        /// Bindable property for the Color property
+        /// </summary>
         public static readonly DependencyProperty ColorProperty =
-            DependencyProperty.Register("Color", typeof(Color), typeof(ColorMap), new PropertyMetadata(new Color(), ColorChanged));
-        private bool isloaded;
+            DependencyProperty.Register("Color", typeof(Color), typeof(ColorMap), new PropertyMetadata(default(Color), OnColorChanged));
 
-        private static void ColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var map = d as ColorMap;
-            if (map != null && !map._settingColor)
+            map?.ColorChanged?.Invoke(map, new ColorChangedEventArgs((Color)e.NewValue));
+
+            if (map == null || map._settingColor)
             {
-                var col = (Color)e.NewValue;
-                var hsl = ColorHelper.ToHSL(col);
-
-                map._settingLightness = true;
-                //map.LightnessSlider.Value = hsl.Z;
-                map._settingLightness = false;
-                map._lightnessMid.Color = ColorHelper.FromHSL(new Vector4(hsl.X, 1, 0.5f, 1));
-
-                double angle = Math.PI * 2 * hsl.X;
-                Vector2 normalized = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-                Vector2 halfSize = new Vector2(
-                    (float)map.ellipse.ActualWidth / 2,
-                    (float)map.ellipse.ActualHeight / 2);
-                Vector2 pos = (hsl.Y / 2) * normalized * halfSize * new Vector2(1, -1) + halfSize;
-
-                map.colorX = pos.X;
-                map.colorY = pos.Y;
-                map.UpdateThumb();
+                return;
             }
+
+            var col = (Color)e.NewValue;
+            var hsl = ColorHelper.ToHSL(col);
+
+            map._lightnessMid.Color = ColorHelper.FromHSL(new Vector4(hsl.X, 1, 0.5f, 1));
+
+            double angle = Math.PI * 2 * hsl.X;
+            Vector2 normalized = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+            Vector2 halfSize = new Vector2(
+                (float)map.ellipse.ActualWidth / 2,
+                (float)map.ellipse.ActualHeight / 2);
+
+            Vector2 pos = ((hsl.Y / 2) * normalized * halfSize * new Vector2(1, -1)) + halfSize;
+
+            map._colorX = pos.X;
+            map._colorY = pos.Y;
+            map.UpdateThumb();
         }
 
         private void Image3_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
             ellipse.CapturePointer(e.Pointer);
-            this.lastPoint = e.GetCurrentPoint(ellipse);
-            this.colorX = lastPoint.Position.X;
-            this.colorY = lastPoint.Position.Y;
-            this.UpdateColor();
-            this.UpdateThumb();
+            _lastPoint = e.GetCurrentPoint(ellipse);
+            _colorX = _lastPoint.Position.X;
+            _colorY = _lastPoint.Position.Y;
+            UpdateColor();
+            UpdateThumb();
             e.Handled = true;
         }
 
         private void Image3_PointerReleased(object sender, PointerRoutedEventArgs e)
         {
             ellipse.ReleasePointerCapture(e.Pointer);
-            this.lastPoint = null;
+            _lastPoint = null;
             e.Handled = true;
         }
 
         private void Image3_PointerMoved(object sender, PointerRoutedEventArgs e)
         {
-            if (ellipse.PointerCaptures?.Any(p => p.PointerId == e.Pointer.PointerId) == true)
+            if (ellipse.PointerCaptures?.Any(p => p.PointerId == e.Pointer.PointerId) != true)
             {
-                this.lastPoint = e.GetCurrentPoint(ellipse);
-                this.colorX = lastPoint.Position.X;
-                this.colorY = lastPoint.Position.Y;
-                var bounds = new Rect(0, 0, ellipse.ActualWidth, ellipse.ActualHeight);
-                if (bounds.Contains(lastPoint.Position) && UpdateColor())
-                {
-                    UpdateThumb();
-                    e.Handled = true;
-                }
+                return;
             }
+
+            _lastPoint = e.GetCurrentPoint(ellipse);
+            _colorX = _lastPoint.Position.X;
+            _colorY = _lastPoint.Position.Y;
+
+            var bounds = new Rect(0, 0, ellipse.ActualWidth, ellipse.ActualHeight);
+            if (!bounds.Contains(_lastPoint.Position) || !UpdateColor())
+            {
+                return;
+            }
+
+            UpdateThumb();
+            e.Handled = true;
         }
 
         private void UpdateThumb()
         {
-            Canvas.SetLeft(thumb, this.colorX - thumb.ActualWidth / 2);
-            Canvas.SetTop(thumb, this.colorY - thumb.ActualHeight / 2);
+            Canvas.SetLeft(thumb, _colorX - (thumb.ActualWidth / 2));
+            Canvas.SetTop(thumb, _colorY - (thumb.ActualHeight / 2));
             thumb.Visibility = Visibility.Visible;
         }
 
         private bool UpdateColor()
         {
-            if (!this.isloaded) return false;
-            var x = this.colorX / ellipse.ActualWidth;
-            var y = 1 - this.colorY / ellipse.ActualHeight;
-            var selectedColor = CalcWheelColor((float)x, 1 - (float)y, 100);
-
-            if (selectedColor.A > 0)
+            if (!_isloaded)
             {
-                this.SetColor(selectedColor);
-                this._lightnessStart.Color = Colors.White;
-                this._lightnessMid.Color = CalcWheelColor((float)x, 1 - (float)y, 0.5f);
-                this._lightnessEnd.Color = Colors.Black;
-                return true;
+                return false;
             }
 
-            return false;
+            var x = _colorX / ellipse.ActualWidth;
+            var y = 1 - (_colorY / ellipse.ActualHeight);
+            var selectedColor = CalcWheelColor((float)x, 1 - (float)y, 100);
+
+            if (selectedColor.A <= 0)
+            {
+                return false;
+            }
+
+            SetColor(selectedColor);
+            _lightnessStart.Color = Colors.White;
+            _lightnessMid.Color = CalcWheelColor((float)x, 1 - (float)y, 0.5f);
+            _lightnessEnd.Color = Colors.Black;
+            return true;
         }
 
         private void SetColor(Color color)
         {
-            this._settingColor = true;
-            this.Color = color;
-            this._settingColor = false;
+            _settingColor = true;
+            Color = color;
+            _settingColor = false;
         }
 
         private async void MeshCanvas_Loaded(object sender, RoutedEventArgs e)
         {
-            bmp3 = new WriteableBitmap(1000, 1000);
+            _bitmap = new WriteableBitmap(1000, 1000);
             await CreateHueCircle(0.5f);
-            this.image3.ImageSource = bmp3;
-            this.isloaded = true;
+            image3.ImageSource = _bitmap;
+            _isloaded = true;
         }
 
         private Task CreateHueCircle(float lightness)
         {
-            return FillBitmap(bmp3, (x, y) =>
-            {
-                return CalcWheelColor(x, y, lightness);
-            });
+            return FillBitmap(_bitmap, (x, y) => CalcWheelColor(x, y, lightness));
         }
 
-        public static Color CalcWheelColor(float x, float y, float lightness)
+        private Color CalcWheelColor(float x, float y, float lightness)
         {
             x = x - 0.5f;
             y = (1 - y) - 0.5f;
-            float saturation = 2 * (float)Math.Sqrt(x * x + y * y);
+            float saturation = 2 * (float)Math.Sqrt((x * x) + (y * y));
             float hue = y < 0 ?
                 (float)Math.Atan2(-y, -x) + (float)Math.PI :
                 (float)Math.Atan2(y, x);
+
             if (saturation > 1)
             {
                 saturation = 1;
@@ -192,13 +215,7 @@ namespace OpenHAB.Windows.Controls
             return ColorHelper.FromHSL(new Vector4(hue / ((float)Math.PI * 2), saturation, lightness, 1));
         }
 
-        private void lightnessChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            if (_settingLightness) return;
-            this.UpdateColor();
-        }
-
-        private static async Task FillBitmap(WriteableBitmap bmp, Func<float, float, Color> fillPixel)
+        private async Task FillBitmap(WriteableBitmap bmp, Func<float, float, Color> fillPixel)
         {
             var stream = bmp.PixelBuffer.AsStream();
             int width = bmp.PixelWidth;
@@ -210,7 +227,7 @@ namespace OpenHAB.Windows.Controls
                     for (int x = 0; x < height; x++)
                     {
                         var color = fillPixel((float)x / width, (float)y / height);
-                        WriteBGRA(stream, color);
+                        WriteBgra(stream, color);
                     }
                 }
             });
@@ -218,7 +235,7 @@ namespace OpenHAB.Windows.Controls
             bmp.Invalidate();
         }
 
-        private static void WriteBGRA(Stream stream, Color color)
+        private void WriteBgra(Stream stream, Color color)
         {
             stream.WriteByte(color.B);
             stream.WriteByte(color.G);
--- OpenHAB.Windows/Controls/ColorWidget.xaml.cs
+++ OpenHAB.Windows/Controls/ColorWidget.xaml.cs
@@ -1,16 +1,72 @@
-﻿namespace OpenHAB.Windows.Controls
+﻿using System;
+using GalaSoft.MvvmLight.Messaging;
+using OpenHAB.Core.Common;
+using OpenHAB.Core.Messages;
+using Windows.UI;
+using Windows.UI.Xaml;
+
+namespace OpenHAB.Windows.Controls
 {
     /// <summary>
     /// Widget control that represents an OpenHAB text
     /// </summary>
     public sealed partial class ColorWidget : WidgetBase
     {
+        private Color _selectedColor;
+
+        /// <summary>
+        /// Gets or sets the color currently selected in the colorpicker widget
+        /// </summary>
+        public Color SelectedColor
+        {
+            get
+            {
+                return _selectedColor;
+            }
+
+            set
+            {
+                if (_selectedColor == value)
+                {
+                    return;
+                }
+
+                _selectedColor = value;
+                RaisePropertyChanged();
+            }
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ColorWidget"/> class.
         /// </summary>
         public ColorWidget()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var rgbString = Widget.Item.State.Split(',');
+
+            if (rgbString.Length == 0)
+            {
+                return;
+            }
+
+            SelectedColor = Core.Common.ColorHelper.FromHSV(Convert.ToDouble(rgbString[0]), Convert.ToDouble(rgbString[1]), Convert.ToDouble(rgbString[2]));
+        }
+
+        private void ColorMap_OnColorChanged(object sender, ColorChangedEventArgs e)
+        {
+            if (Widget == null)
+            {
+                return;
+            }
+
+            var colorMap = (ColorMap)sender;
+            var hsv = Core.Common.ColorHelper.ToHSV(colorMap.Color);
+            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, $"{hsv.X},{hsv.Y},{hsv.Z}"));
         }
     }
 }
