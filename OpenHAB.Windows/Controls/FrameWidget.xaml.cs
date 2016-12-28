0917a797d7767569000045d897e5d728ef68458e
Nico Vermeir <nico_vermeir@hotmail.com>
2016-12-28 21:42:36 +0100

    Made frame icon smaller and next to label

--- OpenHAB.Windows/Controls/FrameWidget.xaml.cs
+++ OpenHAB.Windows/Controls/FrameWidget.xaml.cs
@@ -1,7 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
-using GalaSoft.MvvmLight.Messaging;
+﻿using GalaSoft.MvvmLight.Messaging;
 using OpenHAB.Core.Messages;
 using OpenHAB.Core.Model;
+using Windows.UI.Xaml.Controls;
 
 namespace OpenHAB.Windows.Controls
 {
