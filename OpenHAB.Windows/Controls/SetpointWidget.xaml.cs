636f7223327268deab2edb3a47bd3fb873fe8c58
NicoVermeir <nico_vermeir@hotmail.com>
2017-02-27 19:16:01 +0100

    50 setpoint widget (#58)
    * Fixes #23 style of settingspage matches app design
    
    * Added support for authorization
    
    * Side menu disappears on phone family to save space
    
    * added setpoint widget. fixes #50

---
+++ OpenHAB.Windows/Controls/SetpointWidget.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using GalaSoft.MvvmLight.Messaging;
+using OpenHAB.Core.Messages;
+using OpenHAB.Core.Model;
+using Windows.UI.Xaml;
+
+namespace OpenHAB.Windows.Controls
+{
+    /// <summary>
+    /// Widget control that represents an OpenHAB switch
+    /// </summary>
+    public sealed partial class SetpointWidget : WidgetBase
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the switch is on or off
+        /// </summary>
+        public bool IsOn { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetpointWidget"/> class.
+        /// </summary>
+        public SetpointWidget()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonUp_Click(object sender, RoutedEventArgs e)
+        {
+            double value = double.Parse(Widget.Item.State);
+            value += Widget.Step;
+
+            if (value > Widget.MaxValue)
+            {
+                value = Widget.MaxValue;
+            }
+
+            Widget.Item.State = value.ToString(CultureInfo.CurrentCulture);
+            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, value.ToString(CultureInfo.InvariantCulture)));
+        }
+
+        private void ButtonDown_Click(object sender, RoutedEventArgs e)
+        {
+            double value = double.Parse(Widget.Item.State);
+            value -= Widget.Step;
+
+            if (value < Widget.MinValue)
+            {
+                value = Widget.MinValue;
+            }
+
+            Widget.Item.State = value.ToString(CultureInfo.CurrentCulture);
+            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, value.ToString(CultureInfo.InvariantCulture)));
+        }
+    }
+}
