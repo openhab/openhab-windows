f8cb7c7b7ab04c6ac0e1579b10c716584bf71d53
NicoVermeir <nico_vermeir@hotmail.com>
2017-03-04 15:41:22 +0100

    62 selection widget (#63)
    * Fixes #23 style of settingspage matches app design
    
    * Added support for authorization
    
    * Side menu disappears on phone family to save space
    
    * Fixes #23 style of settingspage matches app design
    
    * Added selection widget

---
+++ OpenHAB.Windows/Controls/SelectionWidget.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using GalaSoft.MvvmLight.Messaging;
+using OpenHAB.Core.Messages;
+using OpenHAB.Core.Model;
+using Windows.UI.Xaml.Controls;
+
+namespace OpenHAB.Windows.Controls
+{
+    /// <summary>
+    /// Widget control that represents an OpenHAB switch
+    /// </summary>
+    public sealed partial class SelectionWidget : WidgetBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SelectionWidget"/> class.
+        /// </summary>
+        public SelectionWidget()
+        {
+            InitializeComponent();
+            Loaded += SelectionWidget_Loaded;
+        }
+
+        private void SelectionWidget_Loaded(object sender, global::Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            OpenHABWidgetMapping itemState = Widget?.Mappings.FirstOrDefault(_ => _.Command == Widget.Item.State);
+            SelectionComboBox.SelectedItem = itemState;
+        }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OpenHABWidgetMapping mapping = e.AddedItems.FirstOrDefault() as OpenHABWidgetMapping;
+
+            if (mapping == null)
+            {
+                return;
+            }
+
+            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, mapping.Command));
+        }
+    }
+}
