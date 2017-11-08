ec229bca2d1b12122447dd35e17e17cfbc66da41
NicoVermeir <nico_vermeir@hotmail.com>
2017-11-08 18:57:04 +0100

    72 image item not working with data (#103)
    * Added support for local data in image element
    
    * Fixed bug

--- OpenHAB.Windows/Controls/ImageWidget.xaml.cs
+++ OpenHAB.Windows/Controls/ImageWidget.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media.Imaging;
+using WinRTXamlToolkit.Imaging;
 
 namespace OpenHAB.Windows.Controls
 {
@@ -24,19 +26,56 @@ namespace OpenHAB.Windows.Controls
             SetState();
         }
 
-        internal override void SetState()
+        internal override async void SetState()
         {
+            if (Widget.Item != null && Widget.Item.State.StartsWith("data:", StringComparison.OrdinalIgnoreCase))
+            {
+                await SetLocalData();
+            }
+            else
+            {
+                SetUrl();
+            }
+        }
+
+        private async Task SetLocalData()
+        {
+            var image = new BitmapImage();
+            string data = Widget.Item.State.Substring(Widget.Item.State.IndexOf(',') + 1);
+            await image.LoadFromBase64StringAsync(data);
+
+            ThumbImage.Source = image;
+            FullImage.Source = image;
+        }
+
+        private void SetUrl()
+        {
+            string url;
+            if (Widget.Item != null && Widget.Item.State.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+            {
+                url = Widget.Item.State;
+            }
+            else
+            {
+                url = Widget.Url;
+            }
+
             ThumbImage.Source = new BitmapImage(
-                    new Uri(Widget.Url, UriKind.Absolute))
+                    new Uri(url, UriKind.Absolute))
                 { CreateOptions = BitmapCreateOptions.IgnoreImageCache };
 
             FullImage.Source = new BitmapImage(
-                    new Uri(Widget.Url, UriKind.Absolute))
+                    new Uri(url, UriKind.Absolute))
                 { CreateOptions = BitmapCreateOptions.IgnoreImageCache };
         }
 
         private async void ImageWidget_OnTapped(object sender, TappedRoutedEventArgs e)
         {
+            if (Widget.LinkedPage != null)
+            {
+                return;
+            }
+
             await PopupDialog.ShowAsync();
         }
     }
