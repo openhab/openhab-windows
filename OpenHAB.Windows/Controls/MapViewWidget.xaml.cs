c5b4c38056b3ebfcb452f9645ab836e1cf66cf2c
Depechie <Depechie@users.noreply.github.com>
2017-11-17 08:50:13 +0100

    88 Add a map widget (#111)
    * FIrst draft MapWidget
    
    * Simplify widget type selectoin and validation through enum use
    
    * Add full view dialog for MapWidget

---
+++ OpenHAB.Windows/Controls/MapViewWidget.xaml.cs
@@ -0,0 +1,61 @@
+﻿using OpenHAB.Windows.Extensions;
+using System;
+using System.Globalization;
+using System.Linq;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.UI.Xaml.Input;
+
+namespace OpenHAB.Windows.Controls
+{
+    /// <summary>
+    /// Widget control that represents an OpenHAB Map
+    /// </summary>
+    public sealed partial class MapViewWidget : WidgetBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapViewWidget"/> class.
+        /// </summary>
+        public MapViewWidget()
+        {
+            InitializeComponent();
+            PopupDialog.AdjustSize();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SetState();
+        }
+
+        private async void OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            await PopupDialog.ShowAsync();
+        }
+
+        internal override void SetState()
+        {
+            if(!string.IsNullOrEmpty(Widget.Item.State))
+            {
+                var latLong = Widget.Item.State.Split(',');
+                if (latLong?.Count() == 2)
+                {
+                    double latitude = double.Parse(latLong[0], CultureInfo.InvariantCulture);
+                    double longitude = double.Parse(latLong[1], CultureInfo.InvariantCulture);
+
+                    MapView.Center = MapViewFull.Center = new Geopoint(new BasicGeoposition() { Latitude = latitude, Longitude = longitude });
+
+                    MapIcon mapIcon = new MapIcon();
+                    mapIcon.Location = MapView.Center;
+                    mapIcon.NormalizedAnchorPoint = new Point(0.5, 0.5);
+                    mapIcon.ZIndex = 0;
+
+                    MapView.MapElements.Add(mapIcon);
+                    MapViewFull.MapElements.Add(mapIcon);
+                }
+            }
+        }
+    }
+}
