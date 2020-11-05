3fa9a62d04524ce88fe50b8a25d9e0c19b6792c7
Christoph Hofmann <oss@hoffe.org>
2020-11-05 23:42:30 +0100

    Fixed conntection profile null reference issue in demo mode (#152)
    Signed-off-by: Christoph Hofmann <oss@hoffe.org>

--- Openhab.Core/Model/Settings.cs
+++ Openhab.Core/Model/Settings.cs
@@ -70,6 +70,7 @@ namespace OpenHAB.Core.Model
                     _connectionProfiles.Add(new DefaultConnectionProfile());
                     _connectionProfiles.Add(new RemoteConnectionProfile());
                     _connectionProfiles.Add(new CloudConnectionProfile());
+                    _connectionProfiles.Add(new DemoConnectionProfile());
                 }
 
                 return _connectionProfiles;
