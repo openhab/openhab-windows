2633cafc201c4d4f63b43b84c90c21d476ec2456
cd-tronic <stuff@cd-tronic.de>
2020-11-20 19:33:59 +0100

    Added Model/OpenHABCommandDescription.cs and Model/OpenHABCommandOptions.cs to branch
    Signed-off-by: cd-tronic <stuff@cd-tronic.de>

---
+++ Openhab.Core/Model/OpenHABCommandDescription.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace OpenHAB.Core.Model
+{
+    /// <summary>
+    /// A mapping for an OpenHAB Widget
+    /// </summary>
+    public class OpenHABCommandDescription
+    {
+        /// <summary>
+        /// Gets or sets the CommandOptions
+        /// </summary>
+        public ICollection<OpenHABCommandOptions> CommandOptions { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenHABWidgetMapping"/> class.
+        /// </summary>
+        /// <param name="command">A command</param>
+        /// <param name="label">A label</param>
+        public OpenHABCommandDescription(ICollection<OpenHABCommandOptions> commandoptions)
+        {
+            CommandOptions = commandoptions;
+        }
+    }
+}
