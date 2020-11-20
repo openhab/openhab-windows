5871b425592ed800dbefa07dfe6e0328d4f81c5d
cd-tronic <stuff@cd-tronic.de>
2020-11-20 15:34:43 +0100

    Added some Changes to enable using CommandOptions. Added commandDescription and commandOption to OpenHABItem.cs to enable json parsing Added subclass to SelectionWidget to handle multiple option sources
    Signed-off-by: cd-tronic <stuff@cd-tronic.de>

--- Openhab.Core/Model/OpenHABItem.cs
+++ Openhab.Core/Model/OpenHABItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using GalaSoft.MvvmLight;
@@ -80,6 +81,12 @@ namespace OpenHAB.Core.Model
         public string Link { get; set; }
 
         /// <summary>
+        /// Gets or sets the CommandDescription of the OpenHAB item
+        /// </summary>
+        ///
+        public OpenHABCommandDescription commandDescription { get; set; }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="OpenHABItem"/> class.
         /// </summary>
         public OpenHABItem()
@@ -121,6 +128,7 @@ namespace OpenHAB.Core.Model
             GroupType = item.GroupType;
             State = item.State;
             Link = item.Link;
+            commandDescription = item.commandDescription;
         }
 
         private void ParseNode(XElement startNode)
@@ -136,6 +144,7 @@ namespace OpenHAB.Core.Model
             State = startNode.Element("state")?.Value;
             Link = startNode.Element("link")?.Value;
         }
+
 
         public void UpdateValue (object value)
         {
