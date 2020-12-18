9b8564b21315ff500f9e89bf018225ce1e819d9f
Christoph Hofmann <oss@hoffe.org>
2020-12-18 11:09:38 +0100

    Fixed connection to demo system and introduced first changes for OH3 (#161)
    Signed-off-by: Christoph Hofmann <oss@hoffe.org>

---
+++ Openhab.Core/Model/OpernAPIInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace OpenHAB.Core.Model
+{
+    /// <summary>
+    /// Response for REST Root call, which provides all information about openHAB instance.
+    /// </summary>
+    public partial class OpenHabAPIInfo
+    {
+        [JsonProperty("version")]
+        public int Version
+        {
+            get; set;
+        }
+
+        [JsonProperty("locale")]
+        public string Locale
+        {
+            get; set;
+        }
+
+        [JsonProperty("measurementSystem")]
+        public string MeasurementSystem
+        {
+            get; set;
+        }
+
+        [JsonProperty("runtimeInfo")]
+        public RuntimeInfo RuntimeInfo
+        {
+            get; set;
+        }
+
+        [JsonProperty("links")]
+        public List<Link> Links
+        {
+            get; set;
+        }
+    }
+}
