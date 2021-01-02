8d19efc9932de1840e2df9a6f5d2270fa1c1f1fb
Christoph Hofmann <oss@hoffe.org>
2021-01-02 11:14:49 +0100

    Adjust menu based on target platform
    Signed-off-by: Christoph Hofmann <oss@hoffe.org>

---
+++ Openhab.Core/Common/DeviceTypeHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+
+namespace OpenHAB.Core.Common
+{
+    public static class DeviceTypeHelper
+    {
+        static DeviceTypeHelper()
+        {
+            DeviceFamily = RecognizeDeviceFamily(Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily);
+        }
+
+        public static DeviceFamily DeviceFamily
+        {
+            get;
+        }
+
+        private static DeviceFamily RecognizeDeviceFamily(string deviceFamily)
+        {
+            switch (deviceFamily)
+            {
+                case "Windows.Mobile":
+                    return DeviceFamily.Mobile;
+                case "Windows.Desktop":
+                    return DeviceFamily.Desktop;
+                case "Windows.Xbox":
+                    return DeviceFamily.Xbox;
+                case "Windows.Holographic":
+                    return DeviceFamily.Holographic;
+                case "Windows.IoT":
+                    return DeviceFamily.IoT;
+                case "Windows.Team":
+                    return DeviceFamily.Team;
+                default:
+                    return DeviceFamily.Unidentified;
+            }
+        }
+    }
+}
