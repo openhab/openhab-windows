c5f7f2364955af2610f588a061f43af736f12d4e
Carlo Dischler <stuff@cd-tronic.de>
2021-01-07 22:04:52 +0100

    Drop down for selection (#156)
    This PR changes the Setpoint-Widget to enable directly switching to a value inside min/max range.
    I changed the simple TextBlock to a ComboBox and added the handling around.
    
    Signed-off-by: cd-tronic stuff@cd-tronic.de

--- OpenHAB.Windows/Controls/SetpointWidget.xaml.cs
+++ OpenHAB.Windows/Controls/SetpointWidget.xaml.cs
@@ -2,6 +2,11 @@
 using GalaSoft.MvvmLight.Messaging;
 using OpenHAB.Core.Messages;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Controls;
+using System;
+using Microsoft.Toolkit.Uwp.Helpers;
+using System.Linq;
 
 namespace OpenHAB.Windows.Controls
 {
@@ -13,6 +18,9 @@ namespace OpenHAB.Windows.Controls
         /// <summary>
         /// Initializes a new instance of the <see cref="SetpointWidget"/> class.
         /// </summary>
+
+        private float step; // represents the stepwidth of the item
+
         public SetpointWidget()
         {
             InitializeComponent();
@@ -20,12 +28,6 @@ namespace OpenHAB.Windows.Controls
 
         private void ButtonUp_Click(object sender, RoutedEventArgs e)
         {
-            // if Widget Step == 0 --> Step = 1
-            float step = Widget.Step;
-            if (step == 0)
-            {
-                step = 1;
-            }
 
             double value = Widget.Item.GetStateAsDoubleValue();
             value += step;
@@ -37,17 +39,11 @@ namespace OpenHAB.Windows.Controls
 
             Widget.Item.UpdateValue(value);
             RaisePropertyChanged(nameof(Widget));
+            SetState();
         }
 
         private void ButtonDown_Click(object sender, RoutedEventArgs e)
         {
-            // if Widget Step == 0 --> Step = 1
-            float step = Widget.Step;
-            if (step == 0)
-            {
-                step = 1;
-            }
-
             double value = Widget.Item.GetStateAsDoubleValue();
             value -= step;
 
@@ -58,10 +54,65 @@ namespace OpenHAB.Windows.Controls
 
             Widget.Item.UpdateValue(value);
             RaisePropertyChanged(nameof(Widget));
+            SetState();
         }
 
         internal override void SetState()
         {
+            DispatcherHelper.ExecuteOnUIThreadAsync(() =>
+            {
+                comboBox.SelectionChanged -= ComboBox_SelectionChanged;
+                UpdateComboBox();
+                comboBox.SelectedItem = Widget.Item.GetStateAsDoubleValue().ToString(CultureInfo.InvariantCulture) + Widget.Item.Unit;
+                comboBox.SelectionChanged += ComboBox_SelectionChanged;
+            });
+
+        }
+
+        private void SetPointWidget_Loaded(object sender, RoutedEventArgs e)
+        {
+            // if Widget Step == 0 --> Step = 1
+            step = Widget.Step;
+            if (step == 0)
+            {
+                step = 1;
+            }
+
+            SetState();
+        }
+
+        private void UpdateComboBox()
+        {
+            comboBox.Items.Clear();
+            float step = 1;
+            double currentValue = Widget.Item.GetStateAsDoubleValue();
+            if (Widget.Step != 0)
+            {
+                step = Widget.Step;
+            }
+            for (float i = Widget.MinValue; i <= Widget.MaxValue; i += step)
+            {
+                comboBox.Items.Add(i.ToString(CultureInfo.InvariantCulture) + Widget.Item.Unit);
+                if (i < currentValue &&  currentValue < (i + step))
+                {
+                    comboBox.Items.Add(currentValue.ToString(CultureInfo.InvariantCulture) + Widget.Item.Unit);
+                }
+            }
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string newValue = (string)e.AddedItems.FirstOrDefault();
+            if (Widget.Item.Unit != null)
+            {
+                newValue = newValue.Replace(Widget.Item.Unit, string.Empty, StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            if (newValue != null)
+            {
+                Widget.Item.UpdateValue(newValue);
+            }
         }
     }
 }
