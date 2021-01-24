5ff22e84c7fdd8fad8b06ec067ee5e8bcb154036
Christoph Hofmann <oss@hoffe.org>
2021-01-24 21:57:19 +0100

    Added missing file
    Signed-off-by: Christoph Hofmann <oss@hoffe.org>

---
+++ OpenHAB.Windows/Controls/DeviceFamilyStateTrigger.cs
@@ -0,0 +1,33 @@
+﻿using OpenHAB.Core.Common;
+using Windows.UI.Xaml;
+
+namespace OpenHAB.Windows.Controls
+{
+    /// <summary>
+    /// Trigger to differentiate between device families
+    /// </summary>
+    public class DeviceFamilyStateTrigger : StateTriggerBase
+    {
+        public static readonly DependencyProperty TargetDeviceFamilyProperty = DependencyProperty.Register(
+            "TargetDeviceFamily", typeof(DeviceFamily), typeof(DeviceFamilyStateTrigger), new PropertyMetadata(default(DeviceFamily), OnDeviceTypePropertyChanged));
+
+        public DeviceFamily TargetDeviceFamily
+        {
+            get
+            {
+                return (DeviceFamily)GetValue(TargetDeviceFamilyProperty);
+            }
+            set
+            {
+                SetValue(TargetDeviceFamilyProperty, value);
+            }
+        }
+
+        private static void OnDeviceTypePropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var trigger = (DeviceFamilyStateTrigger)dependencyObject;
+            var newTargetDeviceFamily = (DeviceFamily)eventArgs.NewValue;
+            trigger.SetActive(newTargetDeviceFamily == DeviceTypeHelper.DeviceFamily);
+        }
+    }
+}
