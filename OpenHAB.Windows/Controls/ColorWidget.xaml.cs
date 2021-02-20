73971a1f8b19395f323db21e4ac71e13997ae13f
cd-tronic <stuff@cd-tronic.de>
2021-02-20 21:11:59 +0100

    Changed Handling of Color Changed in ColorWidget
    Signed-off-by: cd-tronic <stuff@cd-tronic.de>

--- OpenHAB.Windows/Controls/ColorWidget.xaml.cs
+++ OpenHAB.Windows/Controls/ColorWidget.xaml.cs
@@ -14,7 +14,6 @@ namespace OpenHAB.Windows.Controls
     public sealed partial class ColorWidget : WidgetBase
     {
         private Color _selectedColor;
-        private double _brightness;
         /// <summary>
         /// Initializes a new instance of the <see cref="ColorWidget"/> class.
         /// </summary>
@@ -39,8 +38,7 @@ namespace OpenHAB.Windows.Controls
                 }
 
                 _selectedColor = value;
-                var hsvclr = Microsoft.Toolkit.Uwp.Helpers.ColorHelper.ToHsv(_selectedColor);
-                Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, $"{hsvclr.H.ToString(CultureInfo.InvariantCulture)},{(hsvclr.S*100).ToString(CultureInfo.InvariantCulture)}, {((hsvclr.V)*100).ToString(CultureInfo.InvariantCulture)}"));
+                
                 RaisePropertyChanged();
             }
         }
@@ -48,30 +46,31 @@ namespace OpenHAB.Windows.Controls
 
         internal override void SetState()
         {
+            ClrPicker.ColorChanged -= ClrPicker_ColorChanged;
             var rgbString = Widget.Item?.State.Split(',');
 
             if (rgbString == null || rgbString.Length == 0)
             {
                 return;
             }
+            double h = Convert.ToDouble(rgbString[0], CultureInfo.InvariantCulture);
+            double s = Convert.ToDouble(rgbString[1], CultureInfo.InvariantCulture) /100;
+            double v = Convert.ToDouble(rgbString[2], CultureInfo.InvariantCulture) / 100;
 
-            //SelectedColor = Core.Common.ColorHelper.FromHSV(Convert.ToDouble(rgbString[0]), Convert.ToDouble(rgbString[1]), Convert.ToDouble(rgbString[2]));
-        }
-
-        private void ColorMap_OnColorChanged(object sender, ColorChangedEventArgs e)
-        {
-            if (Widget == null)
-            {
-                return;
-            }
-
-            var colorMap = (ColorMap)sender;
-            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, $"{colorMap.Angle.ToString(CultureInfo.InvariantCulture)},{colorMap.RadialPos.ToString(CultureInfo.InvariantCulture)}, 100"));
+            SelectedColor = Microsoft.Toolkit.Uwp.Helpers.ColorHelper.FromHsv(h,s,v);
+            ClrPicker.ColorChanged += ClrPicker_ColorChanged;
         }
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
             SetState();
+            
+        }
+
+        private void ClrPicker_ColorChanged(global::Windows.UI.Xaml.Controls.ColorPicker sender, global::Windows.UI.Xaml.Controls.ColorChangedEventArgs args)
+        {
+            var hsvclr = Microsoft.Toolkit.Uwp.Helpers.ColorHelper.ToHsv(sender.Color);
+            Messenger.Default.Send(new TriggerCommandMessage(Widget.Item, $"{hsvclr.H.ToString(CultureInfo.InvariantCulture)},{(hsvclr.S*100).ToString(CultureInfo.InvariantCulture)}, {((hsvclr.V)*100).ToString(CultureInfo.InvariantCulture)}"));
         }
     }
 }
