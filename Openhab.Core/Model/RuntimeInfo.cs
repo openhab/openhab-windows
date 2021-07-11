6f4fc98d6c01724b0a60e3a60a691b0e8e25e3ff
Christoph Hofmann <oss@hoffe.org>
2021-07-11 12:57:19 +0200

    Updated dependencies and minor fixes (#197)
    - Updated dependencies 
    - Fixed warnings
    - Minor changes
    
    Signed-off-by: Christoph Hofmann <oss@hoffe.org>

--- Openhab.Core/Model/RuntimeInfo.cs
+++ Openhab.Core/Model/RuntimeInfo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-using System.Globalization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
+﻿using Newtonsoft.Json;
 
 namespace OpenHAB.Core.Model
 {
