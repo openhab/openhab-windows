e6b74483885626b98b917754e893ad0432bb2ef6
Timothy Clark <rebeltat@rebeldesigns.net>
2022-02-15 21:57:58 -0800

    Update OpenHABAPIInfo.cs
    Signed-off-by: Timothy Clark <rebeltat@rebeldesigns.net>

--- Openhab.Core/Model/OpenHABAPIInfo.cs
+++ Openhab.Core/Model/OpenHABAPIInfo.cs
@@ -6,7 +6,7 @@ namespace OpenHAB.Core.Model
     /// <summary>
     /// Response for REST Root call, which provides all information about openHAB instance.
     /// </summary>
-    public class OpenHABAPIInfo
+    public class OpenHabAPIInfo
     {
         /// <summary>Gets or sets the version.</summary>
         /// <value>The version.</value>
