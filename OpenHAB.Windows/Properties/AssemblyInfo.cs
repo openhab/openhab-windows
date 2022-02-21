709ae3eb36672d9186e36d743e25ed93245c8540
Timothy Clark <rebeltat@rebeldesigns.net>
2022-02-21 02:32:15 -0800

    update config

--- OpenHAB.Windows/Properties/AssemblyInfo.cs
+++ OpenHAB.Windows/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -27,3 +28,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyTitleAttribute("OpenHAB.Windows")]
+[assembly: AssemblyDescriptionAttribute("OpenHAB.Windows")]
+[assembly: AssemblyProductAttribute("OpenHAB.Windows")]
+[assembly: AssemblyCopyrightAttribute("2022")]
+[assembly: AssemblyVersionAttribute("1.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
